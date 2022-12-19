0e0bb9d48793a09738bcc4310e301dee650ff7c5
imen <imenjlassi02@gmail.com>
2022-12-19 18:16:23 +0100

    add check-RestApi

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,11 @@ const router = express.Router();
 const controllers = require("../controllers/userControllers");
 const User = require('../models/User');
 
-//test routing
-router.get('/hello', (req, res)=>{
-    res.send('hello routing..')
-})
-//add user /method post/params Body
+//POST :  ADD A NEW USER TO THE DATABASE 
 //path :http://localhost:7001/api/newUser
 router.post('/newUser', controllers.postUser )
 
-//get users /method get
+//GET :  RETURN ALL USERS 
 //path : http://localhost:7001/api/users
 router.get("/users",async(req,res)=>{
     try {
@@ -22,38 +18,7 @@ router.get("/users",async(req,res)=>{
     }
 })
 
-//get One user /method get
-//path :http://localhost:7001/api/users/:id
-router.get("/users/:id",async(req,res)=>{
-    try {
-        const result = await User.findOne({_id:req.params.id})
-        if(result){
-            res.status(200).send({response:result , message:"geting user by ID seccessfully"})
-        }else{
-            res.status(400).send({message:"there is no user with this id"})
-        }
-    } catch (error) {
-        res.send({message:"can not get user"})
-    }
-})
-
-//Delete  user /method delete
-//path :http://localhost:7001/api/delete/:id
-router.delete("/delete/:id",async(req,res)=>{
-    try {
-        const result = await User.deleteOne({_id:req.params.id})
-        if(result){
-            res.status(200).send({response:result , message:"delete user by ID seccessfully"})
-        }else{
-            res.status(400).send({message:"there is no user with this id"})
-        }
-
-    } catch (error) {
-        res.send({message:"can not delete user"})
-    }
-})
-
-//Update One user /method put
+//PUT : EDIT A USER BY ID 
 //path :http://localhost:7001/api/user/:id
 router.put("/user/:id",async(req,res)=>{
     try {
@@ -67,6 +32,22 @@ router.put("/user/:id",async(req,res)=>{
     } catch (error) {
         console.log(error)
         res.send({message:"can not update user"})
+    }
+})
+
+//DELETE : REMOVE A USER BY ID 
+//path :http://localhost:7001/api/delete/:id
+router.delete("/delete/:id",async(req,res)=>{
+    try {
+        const result = await User.deleteOne({_id:req.params.id})
+        if(result){
+            res.status(200).send({response:result , message:"delete user by ID seccessfully"})
+        }else{
+            res.status(400).send({message:"there is no user with this id"})
+        }
+
+    } catch (error) {
+        res.send({message:"can not delete user"})
     }
 })
 
